dc16d9b7ff1426c632d9fb6ee6e1c25abf664c64
Tomas Camin <tomas.camin@scmitaly.it>
2018-08-30 09:22:49 +0200

    Add switch command template

---
+++ lib/pod_builder/command/switch.rb
@@ -0,0 +1,33 @@
+require 'pod_builder/core'
+
+module PodBuilder
+  module Command
+    class Switch
+      def self.call(options)
+        Configuration.check_inited
+
+        argument_pods = ARGV.dup
+
+        unless argument_pods.count > 0 
+          return false
+        end
+        unless argument_pods.count == 1
+          raise "\nSpecify a single pod to switch\n\n".red 
+          return false
+        end
+
+        check_not_building_subspec(argument_pods.first)
+
+        raise "FIXME: TODO"
+      end
+
+      private 
+
+      def self.check_not_building_subspec(pod_to_switch)
+        if pod_to_switch.include?("/")
+          raise "\nCan't switch subspec #{pod_to_switch} refer to podspec name.\n\nUse `pod_builder switch #{pod_to_switch.split("/").first}` instead\n\n".red
+        end
+      end
+    end
+  end    
+end
