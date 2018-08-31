c09697753b9bd1dd1fed43bb74e3d2daa647532b
Tomas Camin <tomas.camin@scmitaly.it>
2018-08-31 11:03:21 +0200

    Rewrite local podspec generation

--- lib/pod_builder/podspec.rb
+++ lib/pod_builder/podspec.rb
@@ -1,53 +1,36 @@
 module PodBuilder
   class Podspec
-    def self.generate     
-      buildable_items = Podfile.podfile_items_at(PodBuilder::basepath("Podfile"))
+    class PodspecItem
+      attr_accessor :name
+      attr_accessor :module_name
+      attr_accessor :vendored_frameworks
+      attr_accessor :frameworks
+      attr_accessor :libraries
+      attr_accessor :resources
+      attr_accessor :exclude_files
 
-      grouped_buildable_items = buildable_items.select { |x| x.is_subspec }.group_by { |x| x.root_name }
-
-      grouped_buildable_items.each do |root_name, subspecs|
-        subspecs.select! { |x| !Configuration.subspecs_to_split.include?(x.name) }
-        unless !buildable_items.map(&:name).include?(root_name) && subspecs.count > 0
-          next
-        end
-
-        spec_raw = {}
-        all_specs = []
-        checkout_options = {}
-        
-        spec_raw["name"] = root_name
-        spec_raw["module_name"] = root_name
-
-        # spec_raw["source"] = ["git": "https://www.subito.it/pod/#{root_name}.git", "tag": "1.0"]
-        spec_raw["source"] = []
-
-        deps = subspecs.map { |x| x.dependencies(buildable_items) }.flatten.uniq
-        spec_raw["dependencies"] = deps.map { |x| [x.root_name, []] }.to_h
-        
-        spec_raw["static_framework"] = subspecs.map(&:is_static).reduce(true) { |result, item| result && item }
-
-        xcconfig = {}
-        subspecs.map(&:xcconfig).each do |xconfigs|
-          xconfigs.each do |key, value|
-            if xcconfig[key].nil?
-              xcconfig[key] = value
-            elsif !xcconfig[key].include?(value)
-              xcconfig[key] << " #{value}"
-            end
-          end
-        end
-
-        spec_raw["xcconfig"] = xcconfig
-
-        spec = Pod::Specification.from_string(spec_raw.to_json, "podspec.json")        
-
-        pod = PodfileItem.new(spec, all_specs, checkout_options)
-        buildable_items.push(pod)
+      def initialize
+        @name = ""
+        @module_name = ""
+        @vendored_frameworks = []
+        @frameworks = []
+        @libraries = []
+        @resources = []
+        @exclude_files = []
       end
 
-      buildable_items.sort_by! { |x| x.name }
+      def to_s
+        @name
+      end
+    end
+    private_constant :PodspecItem
 
-      podspecs = []
+    def self.generate     
+      buildable_items = Podfile.podfile_items_at(PodBuilder::basepath("Podfile")).sort_by { |x| x.name }
+      all_specs = buildable_items.map { |x| x.pod_specification(buildable_items) }
+
+      podspec_items = []
+
       buildable_items.each do |pod|
         spec_exists = File.exist?(PodBuilder::basepath(vendored_spec_framework_path(pod))) 
         subspec_exists = File.exist?(PodBuilder::basepath(vendored_subspec_framework_path(pod)))
@@ -57,30 +40,45 @@ module PodBuilder
           next
         end
 
-        vendored_frameworks = [pod] + pod.dependencies(buildable_items)
+        pod_name = Configuration.subspecs_to_split.include?(pod.name) ? pod.name : pod.root_name
+        unless podspec_item = podspec_items.detect { |x| x.name == pod_name }
+          podspec_item = PodspecItem.new
+          podspec_items.push(podspec_item)
+          podspec_item.name = pod_name
+          podspec_item.module_name = pod.module_name
+        end
+        
+        podspec_item.vendored_frameworks += [pod] + pod.dependencies(buildable_items)
 
-        static_vendored_frameworks = vendored_frameworks.select { |x| x.is_static }
-        framework_paths = vendored_frameworks.map { |x| vendored_framework_path(x) }.compact
-        
-        podspec = "  s.subspec '#{pod.podspec_name}' do |p|\n"
-        podspec += "    p.vendored_frameworks = '#{framework_paths.uniq.join("','")}'\n"
-        
-        podspec_resources = static_vendored_frameworks.map { |x| "#{vendored_framework_path(x)}/*.{nib,bundle,xcasset,strings,png,jpg,tif,tiff,otf,ttf,ttc,plist,json,caf,wav,p12,momd}" }
-        if podspec_resources.count > 0
-          podspec += "    p.resources = '#{podspec_resources.uniq.join("','")}'\n"
-        end
-        
-        podspec_exclude_files = static_vendored_frameworks.map { |x| "#{vendored_framework_path(x)}/Info.plist" }
-        if podspec_exclude_files.count > 0
-          podspec += "    p.exclude_files = '#{podspec_exclude_files.uniq.join("','")}'\n"
-        end
+        podspec_item.frameworks = podspec_item.vendored_frameworks.map { |x| x.frameworks }.flatten.uniq.sort     
+        podspec_item.libraries = podspec_item.vendored_frameworks.map { |x| x.libraries }.flatten.uniq.sort
 
-        if pod.xcconfig.count > 0
-          podspec += "    p.xcconfig = #{pod.xcconfig.to_s.gsub("\"", "'")}\n"
-        end
+        static_vendored_frameworks = podspec_item.vendored_frameworks.select { |x| x.is_static }
         
+        podspec_item.resources = static_vendored_frameworks.map { |x| "#{vendored_framework_path(x)}/*.{nib,bundle,xcasset,strings,png,jpg,tif,tiff,otf,ttf,ttc,plist,json,caf,wav,p12,momd}" }.flatten.uniq
+        podspec_item.exclude_files = static_vendored_frameworks.map { |x| "#{vendored_framework_path(x)}/Info.plist" }.flatten.uniq
+      end
+
+      podspecs = []
+      podspec_items.each do |item|
+        vendored_frameworks = item.vendored_frameworks.map { |x| vendored_framework_path(x) }.compact.uniq
+
+        podspec = "  s.subspec '#{item.name.gsub("/", "_")}' do |p|\n"
+        podspec += "    p.vendored_frameworks = '#{vendored_frameworks.join("','")}'\n"
+        if item.frameworks.count > 0
+          podspec += "    p.frameworks = '#{item.frameworks.join("', '")}'\n"
+        end
+        if item.libraries.count > 0
+          podspec += "    p.libraries = '#{item.libraries.join("', '")}'\n"
+        end
+        if item.resources.count > 0
+          podspec += "    p.resources = '#{item.resources.join("', '")}'\n"
+        end
+        if item.resources.count > 0
+          podspec += "    p.exclude_files = '#{item.exclude_files.join("', '")}'\n"
+        end
         podspec += "  end"
-        
+
         podspecs.push(podspec)
       end
       
