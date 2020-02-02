574764aee290b5b5e8ac76677ebe1464a0808023
Tomas Camin <tomas.camin@adevinta.com>
2020-02-02 13:47:12 +0100

    Rewrite leveraging original_compile_path

--- lib/pod_builder/command/update_lldbinit.rb
+++ lib/pod_builder/command/update_lldbinit.rb
@@ -18,32 +18,35 @@ module PodBuilder
             
         base_path = PodBuilder::basepath("")
 
+        podfile_restore_content = File.read(PodBuilder::basepath("Podfile.restore"))
+        app_podfile_content = File.read(PodBuilder::project_path("Podfile"))
+
         lldbinit_path = File.expand_path('~/.lldbinit-Xcode')
         lldbinit_content = File.exists?(lldbinit_path) ? File.read(lldbinit_path) : ""
-        restore_hash = podfile_restore_hash()
-        if lldbinit_content.include?("# <pb_md5:#{base_path}:#{restore_hash}")
+        status_hash = podfiles_status_hash(app_podfile_content, podfile_restore_content)
+        if lldbinit_content.include?("# <pb_md5:#{base_path}:#{status_hash}")
           puts "\n\n🎉 already in sync!\n".green
           return 0
         end
 
-        path = argument_pods[0]
+        source_path = argument_pods[0]
         
-        is_absolute = ["~", "/"].include?(path[0])
+        is_absolute = ["~", "/"].include?(source_path[0])
         if !is_absolute
-          path = Pathname.new(File.join(base_path, path))
+          source_path = Pathname.new(File.join(base_path, source_path))
         end
         
-        path = File.expand_path(path)
+        source_path = File.expand_path(source_path)
         
         framework_paths = Dir.glob("#{base_path}/**/*.framework")
         
         unless framework_paths.count > 0
-          raise "\n\nNo prebuilt frameworks found in `#{path}`\n\n".red 
+          raise "\n\nNo prebuilt frameworks found in `#{framework_paths}`\n\n".red 
         end
 
         puts "Extracting debug information".yellow
 
-        podspec_paths = Dir.glob("#{path}/**/*.podspec") + Dir.glob("#{path}/**/*.podspec.json")
+        podspec_paths = Dir.glob("#{source_path}/**/*.podspec") + Dir.glob("#{source_path}/**/*.podspec.json")
         podspec_contents = podspec_paths.map { |t| File.read(t).gsub(/\s+/, "").gsub("\"", "'") }
         
         replace_paths = []
@@ -51,47 +54,48 @@ module PodBuilder
         framework_paths.each do |framework_path|
           name = File.basename(framework_path, File.extname(framework_path)) 
           executable_path = File.join(framework_path, name)
+
+          podbuilder_plist = File.join(framework_path, Configuration.framework_plist_filename)
+
+          plist = CFPropertyList::List.new(:file => podbuilder_plist)
+          data = CFPropertyList.native_types(plist.value)
+
+          original_compile_path = data["original_compile_path"]
+          is_prebuilt = data.fetch("is_prebuilt", true)
+
+          if original_compile_path.nil?
+            puts "\n\n#{framework_path} was compiled with an older version of PodBuilder, please rebuild it to update `~/.lldbinit-Xcode`"
+            next
+          end
           
-          dwarf_dump_lib = `dwarfdump --debug-info #{executable_path} | grep '#{Configuration.build_base_path}' | head -n 1`.strip()          
-          
-          if (matches = dwarf_dump_lib.match(/#{Configuration.build_base_path}(.*)\/Pods/)) && matches.size == 2
-            original_compile_path = "#{Configuration.build_base_path}#{matches[1]}/Pods/#{name}"
-            
-            if podspec_path = find_podspec_path_for(name, podspec_paths, podspec_contents)
+          if is_prebuilt
+            next
+          end
+
+          if podspec_path = find_podspec_path_for(name, podspec_paths, podspec_contents)
+            if !is_development_pod(podspec_path, app_podfile_content)
               replace_paths.push("#{original_compile_path},#{File.dirname(podspec_path)}")
             else
-              puts "Failed to find podspec for #{name}".blue
-            end 
+              puts "#{name} is in development pod, skipping"
+            end
           else
-            # Look for path in dSYM
-            dsym_paths = Dir.glob("#{base_path}/**/iphonesimulator/#{name}.framework.dSYM")
-            dsym_paths.each do |dsym_path|
-              name = File.basename(dsym_path, ".framework.dSYM") 
-              dsym_dwarf_path = File.join(dsym_path, "Contents/Resources/DWARF")
-              dsym_dwarf_path = File.join(dsym_dwarf_path, name)
-              
-              dwarf_dump_lib = `dwarfdump --debug-info #{dsym_dwarf_path} | grep '#{Configuration.build_base_path}' | head -n 1`.strip()
-              
-              if (matches = dwarf_dump_lib.match(/#{Configuration.build_base_path}(.*)\/Pods/)) && matches.size == 2
-                original_compile_path = "#{Configuration.build_base_path}#{matches[1]}/Pods/#{name}"
-
-                if podspec_path = find_podspec_path_for(name, podspec_paths, podspec_contents)
-                  replace_paths.push("#{original_compile_path},#{File.dirname(podspec_path)}")
-                else
-                  puts "Failed to find podspec for #{name}".blue
-                end                
-              end
-            end    
-          end
+            puts "Failed to find podspec for #{name}, skipping".blue
+          end   
         end
         
         replace_paths.uniq!
 
         source_map_lines = replace_paths.flat_map { |t| ["# <pb:#{base_path}>\n", "settings append target.source-map '#{t.split(",").first}' '#{t.split(",").last}'\n"] }
-        rewrite_lldinit(source_map_lines, base_path)
+        rewrite_lldinit(source_map_lines, base_path, app_podfile_content, podfile_restore_content)
         
         puts "\n\n🎉 done!\n".green
         return 0
+      end
+
+      def self.is_development_pod(podspec_path, app_podfile_content)
+        development_path = Pathname.new(podspec_path).relative_path_from(Pathname.new(PodBuilder::project_path)).parent.to_s
+
+        return app_podfile_content.include?(":path => '#{development_path}'")
       end
 
       def self.find_podspec_path_for(name, podspec_paths, podspec_contents)
@@ -110,11 +114,14 @@ module PodBuilder
         end
       end
 
-      def self.podfile_restore_hash()
-        Digest::MD5.hexdigest(File.read(PodBuilder::basepath("Podfile.restore")))
+      def self.podfiles_status_hash(app_podfile_content, podfile_restore_content)
+        # Change to either Podfile.restore (which presumely mean new prebuilds done)
+        # or app's Podfile (which my occurr when pods are switched to development pod)
+        # should force a regeneration of the status identifier
+        Digest::MD5.hexdigest(podfile_restore_content + app_podfile_content)
       end
 
-      def self.rewrite_lldinit(source_map_lines, base_path)
+      def self.rewrite_lldinit(source_map_lines, base_path, app_podfile_content, podfile_restore_content)
         puts "Writing ~/.lldbinit-Xcode".yellow
 
         lldbinit_path = File.expand_path('~/.lldbinit-Xcode')
@@ -137,12 +144,12 @@ module PodBuilder
           end
         end
 
-        restore_hash = podfile_restore_hash()
+        status_hash = podfiles_status_hash(app_podfile_content, podfile_restore_content)
 
         source_map_lines.insert(0, "# <pb>\n")
         source_map_lines.insert(1, "settings clear target.source-map\n")
         source_map_lines.insert(2, "# <pb:#{base_path}>\n")
-        source_map_lines.insert(3, "# <pb_md5:#{base_path}:#{restore_hash}>\n")
+        source_map_lines.insert(3, "# <pb_md5:#{base_path}:#{status_hash}>\n")
 
         lldbinit_lines += source_map_lines
       
