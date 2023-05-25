4f9918242c4836ec757b20ce080f920d886528ce
Tomas Camin <tomas.camin@adevinta.com>
2023-05-25 15:46:56 +0200

    Format

--- lib/pod_builder/info.rb
+++ lib/pod_builder/info.rb
@@ -1,4 +1,4 @@
-require 'json'
+require "json"
 
 module PodBuilder
   class Info
@@ -7,57 +7,57 @@ module PodBuilder
       result = {}
       name = nil
 
-      Dir.glob(PodBuilder::prebuiltpath("**/#{Configuration.prebuilt_info_filename}")).each do |json_path|         
+      Dir.glob(PodBuilder::prebuiltpath("**/#{Configuration.prebuilt_info_filename}")).each do |json_path|
         name, prebuilt_info = prebuilt_info(json_path)
         result[name] = prebuilt_info
       end
 
       return result
     end
-    
+
     private
 
     def self.pod_name_from_entry(line)
       if (matches = line&.match(/pod '(.*?)'/)) && matches.size == 2
         pod_name = matches[1]
-        
+
         return pod_name
       end
 
       return "unknown_podname"
     end
-    
+
     def self.version_info_from_entry(line)
       if (matches = line&.match(/pod '(.*)', '=(.*)'/)) && matches.size == 3
         pod_name = matches[1]
         tag = matches[2]
-        
+
         return { "tag": tag }
       elsif (matches = line&.match(/pod '(.*)', :git => '(.*)', :commit => '(.*)'/)) && matches.size == 4
         pod_name = matches[1]
         repo = matches[2]
         hash = matches[3]
-        
+
         return { "repo": repo, "hash": hash }
       elsif (matches = line&.match(/pod '(.*)', :git => '(.*)', :branch => '(.*)'/)) && matches.size == 4
         pod_name = matches[1]
         repo = matches[2]
         branch = matches[3]
-        
+
         return { "repo": repo, "branch": branch }
       elsif (matches = line&.match(/pod '(.*)', :git => '(.*)', :tag => '(.*)'/)) && matches.size == 4
         pod_name = matches[1]
         repo = matches[2]
         tag = matches[3]
-        
+
         return { "repo": repo, "tag": tag }
       elsif (matches = line&.match(/pod '(.*)', :path => '(.*)'/)) && matches.size == 3
         pod_name = matches[1]
-        
+
         return { "repo": "local" }
       elsif (matches = line&.match(/pod '(.*)', :podspec => '(.*)'/)) && matches.size == 3
         pod_name = matches[1]
-        
+
         return { "repo": "local" }
       else
         raise "\n\nFailed extracting version from line:\n#{line}\n".red
@@ -70,12 +70,12 @@ module PodBuilder
       end
 
       data = JSON.load(File.read(path))
-            
+
       result = {}
       if swift_version = data["swift_version"]
-        result.merge!({ "swift_version": swift_version})
+        result.merge!({ "swift_version": swift_version })
       end
-      
+
       pod_version = version_info_from_entry(data["entry"])
       pod_name = pod_name_from_entry(data["entry"])
 
@@ -83,8 +83,8 @@ module PodBuilder
       result.merge!({ "specs": (data["specs"] || []) })
       result.merge!({ "is_static": (data["is_static"] || false) })
       result.merge!({ "original_compile_path": (data["original_compile_path"] || "") })
-      
+
       return pod_name, result
-    end    
+    end
   end
 end
